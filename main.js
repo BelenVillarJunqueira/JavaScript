7f9b2ffe925ead070aa99dc0bff66d275b6eb314
Belen Villar Junqueira <belu.dvt@hotmail.com>
2025-04-20 19:17:44 -0300

    cambios turnero

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-let turnos = [
-    {especialidad: "Clinica Médica", consultorio: 1},
-    {especialidad: "Cardiología", consultorio: 2},
-    {especialidad: "Traumatología", consultorio: 3},
-    {especialidad: "Oftalmología", consultorio: 4},
-    {especialidad: "Ginecología", consultorio: 5}
-];
+let turnos = [];
 
 function sacarTurno() {
     let nombre = prompt("Ingrese su nombre:");
     if (!nombre) {
         alert("Debe ingresar un nombre válido.");
+        return;
+    }
+
+    let edad = prompt("Ingrese su edad:");
+    if (isNaN(edad) || edad <= 0) {
+        alert("Edad no válida.");
+        return;
+    }
+
+    let dni = prompt("Ingrese su DNI:");
+    if (!dni || isNaN(dni)) {
+        alert("DNI no válido.");
+        return;
+    }
+
+    let direccion = prompt("Ingrese su dirección:");
+    if (!direccion) {
+        alert("Debe ingresar una dirección válida.");
+        return;
+    }
+
+    let telefono = prompt("Ingrese su teléfono:");
+    if (!telefono || isNaN(telefono)) {
+        alert("telefono no válido.");
         return;
     }
 
@@ -44,10 +62,26 @@ function atenderTurno() {
     console.log("Lista de turnos después de atender:", turnos);
 }
 
+function filtrarTurnosPorEspecialidad() {
+    let especialidadBuscar = prompt("Ingrese la especialidad a filtrar:");
+    let filtrados = turnos.filter(turno => turno.especialidad.toLowerCase() === especialidadBuscar.toLowerCase());
+
+    if (filtrados.length === 0) {
+        alert("No se encontraron turnos para esa especialidad.");
+    } else {
+        let mensaje = "Turnos encontrados:\n";
+        filtrados.forEach(t => {
+            mensaje += `- ${t.nombre} (DNI: ${t.dni}, Edad: ${t.edad}, (Teléfono: ${t.telefono}, Dirección: ${t.direccion})\n`;
+        });
+        alert(mensaje);
+        console.log("Filtrados:", filtrados);
+    }
+}
+
 function iniciarSimulador() {
     let opcion;
     do {
-        opcion = prompt("Bienvenido al turnero del hospital.\nElija una opción:\n1. Sacar turno\n2. Atender turno\n3. Salir");
+        opcion = prompt("Bienvenido al turnero del hospital.\n Elija una opción:\n1. Sacar turno\n2. Atender turno\n3. Filtrar turnos por especialidad\n4. Salir");
 
         switch (opcion) {
             case "1":
@@ -57,12 +91,15 @@ function iniciarSimulador() {
                 atenderTurno();
                 break;
             case "3":
+                filtrarTurnosPorEspecialidad();
+                break;
+            case "4":
                 alert("Gracias por atenderse con nosotros.");
                 break;
             default:
                 alert("Opción no válida. Inténtelo de nuevo.");
         }
-    } while (opcion !== "3");
+    } while (opcion !== "4");
 }
 
 iniciarSimulador();
